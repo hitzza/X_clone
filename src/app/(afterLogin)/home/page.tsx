6f3e88ccb5e54175eefbdb0b6b7ac00d9c3c2ff0
최재혁 <cslzh4433@gmail.com>
2024-01-18 13:13:40 +0900

    Feat: add home components

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,17 @@
+import Post from "../_component/Post";
+import style from "./home.css';.module.css";
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+
 export default function Home() {
-  return <main>home </main>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+      </TabProvider>
+    </main>
+  );
 }
