bed3dabbe22043e4980e7205534e3d0e97ad6323
최재혁 <cslzh4433@gmail.com>
2024-01-22 15:41:21 +0900

    Feat: add singlepost

--- src/app/(afterLogin)/_component/PostArticle.tsx
+++ src/app/(afterLogin)/_component/PostArticle.tsx
@@ -24,7 +24,7 @@ export default function PostArticle({ children, post }: Props) {
   const onClick = () => {
     router.push(`/${post.User.id}/status/${post.postId}`);
   };
-
+  //서버 컴포넌트 안에서 클라이언트 컴포넌트를 사용하려면 import 하지말고 children으로 받아야함!
   return (
     <article onClickCapture={onClick} className={style.post}>
       {children}
